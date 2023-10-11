7ec6eae66edfdf6bc5e4d823fd2cb8c5d988ed8f
SimovDenis <131714121+SimovDenis@users.noreply.github.com>
2023-10-11 11:59:35 +0200

    UrednijiIzbornik

--- Zavrsni/MotoTrgovinaApp/src/main/java/zavrsni/view/Izbornik.java
+++ Zavrsni/MotoTrgovinaApp/src/main/java/zavrsni/view/Izbornik.java
@@ -21,7 +21,7 @@ public class Izbornik extends javax.swing.JFrame {
      */
     public Izbornik() {
         initComponents();
-        setTitle(Alati.NAZIV_APP + " | IZBORNIK");
+        lblNaslov.setText(Alati.NAZIV_APP + " | IZBORNIK");
         lblOperater.setText(Alati.getOperater());
     }
 
@@ -34,103 +34,130 @@ public class Izbornik extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        btnProizvodi = new javax.swing.JButton();
+        btnDjelatnici = new javax.swing.JButton();
+        btnKupci = new javax.swing.JButton();
+        btnRacuni = new javax.swing.JButton();
+        btnOnama = new javax.swing.JButton();
+        btnIzlaz = new javax.swing.JButton();
+        lblNaslov = new javax.swing.JLabel();
         jToolBar1 = new javax.swing.JToolBar();
         lblOperater = new javax.swing.JLabel();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenuItem3 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
-        jMenuItem5 = new javax.swing.JMenuItem();
-        jMenuItem6 = new javax.swing.JMenuItem();
-        jMenu2 = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        btnProizvodi.setText("Proizvodi");
+        btnProizvodi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnProizvodiActionPerformed(evt);
+            }
+        });
+
+        btnDjelatnici.setText("Djelatnici");
+        btnDjelatnici.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDjelatniciActionPerformed(evt);
+            }
+        });
+
+        btnKupci.setText("Kupci");
+        btnKupci.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnKupciActionPerformed(evt);
+            }
+        });
+
+        btnRacuni.setText("Računi");
+        btnRacuni.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRacuniActionPerformed(evt);
+            }
+        });
+
+        btnOnama.setText("O nama");
+        btnOnama.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnOnamaActionPerformed(evt);
+            }
+        });
+
+        btnIzlaz.setText("Izlaz");
+        btnIzlaz.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnIzlazActionPerformed(evt);
+            }
+        });
+
         jToolBar1.setRollover(true);
         jToolBar1.add(lblOperater);
-
-        jMenu1.setText("Programi");
-
-        jMenuItem3.setText("Proizvodi");
-        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem3ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem3);
-
-        jMenuItem4.setText("Djelatnici");
-        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem4ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem4);
-
-        jMenuItem5.setText("Kupci");
-        jMenuItem5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem5ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem5);
-
-        jMenuItem6.setText("Računi");
-        jMenuItem6.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem6ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem6);
-
-        jMenuBar1.add(jMenu1);
-
-        jMenu2.setText("Ostalo");
-
-        jMenuItem1.setText("O Aplikaciji");
-        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem1ActionPerformed(evt);
-            }
-        });
-        jMenu2.add(jMenuItem1);
-
-        jMenuItem2.setText("Izlaz");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem2ActionPerformed(evt);
-            }
-        });
-        jMenu2.add(jMenuItem2);
-
-        jMenuBar1.add(jMenu2);
-
-        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jToolBar1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(lblNaslov, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(52, 52, 52)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(btnIzlaz, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnDjelatnici, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnProizvodi, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnKupci, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnRacuni, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnOnama, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(0, 53, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jToolBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 252, Short.MAX_VALUE)
-                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap()
+                .addComponent(lblNaslov)
+                .addGap(18, 18, 18)
+                .addComponent(btnProizvodi)
+                .addGap(18, 18, 18)
+                .addComponent(btnDjelatnici)
+                .addGap(18, 18, 18)
+                .addComponent(btnKupci)
+                .addGap(18, 18, 18)
+                .addComponent(btnRacuni)
+                .addGap(18, 18, 18)
+                .addComponent(btnOnama)
+                .addGap(18, 18, 18)
+                .addComponent(btnIzlaz)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
+                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        dispose();
-    }//GEN-LAST:event_jMenuItem2ActionPerformed
+    private void btnProizvodiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProizvodiActionPerformed
+        new ProzorProizvod().setVisible(true);
+    }//GEN-LAST:event_btnProizvodiActionPerformed
 
-    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+    private void btnDjelatniciActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDjelatniciActionPerformed
+        new ProzorDjelatnik().setVisible(true);
+    }//GEN-LAST:event_btnDjelatniciActionPerformed
+
+    private void btnKupciActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnKupciActionPerformed
+        new ProzorKupac().setVisible(true);
+    }//GEN-LAST:event_btnKupciActionPerformed
+
+    private void btnRacuniActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRacuniActionPerformed
+        new ProzorRacun().setVisible(true);
+    }//GEN-LAST:event_btnRacuniActionPerformed
+
+    private void btnOnamaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOnamaActionPerformed
         Image slika = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
         ImageIcon icon = new ImageIcon(slika);
 
@@ -138,36 +165,22 @@ public class Izbornik extends javax.swing.JFrame {
                 "Ovaj projekt nastao je tijekom pohađanja u Edunovi 2023. godine\n"
                 + "Autor je Denis Simov",
                 "Aplikacija Moto Trgovina", JOptionPane.INFORMATION_MESSAGE, icon);
-    }//GEN-LAST:event_jMenuItem1ActionPerformed
+    }//GEN-LAST:event_btnOnamaActionPerformed
 
-    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
-        new ProzorProizvod().setVisible(true);
-    }//GEN-LAST:event_jMenuItem3ActionPerformed
-
-    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
-        new ProzorDjelatnik().setVisible(true);
-    }//GEN-LAST:event_jMenuItem4ActionPerformed
-
-    private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
-        new ProzorKupac().setVisible(true);
-    }//GEN-LAST:event_jMenuItem5ActionPerformed
-
-    private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem6ActionPerformed
-        new ProzorRacun().setVisible(true);
-    }//GEN-LAST:event_jMenuItem6ActionPerformed
+    private void btnIzlazActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIzlazActionPerformed
+        dispose();
+    }//GEN-LAST:event_btnIzlazActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem4;
-    private javax.swing.JMenuItem jMenuItem5;
-    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JButton btnDjelatnici;
+    private javax.swing.JButton btnIzlaz;
+    private javax.swing.JButton btnKupci;
+    private javax.swing.JButton btnOnama;
+    private javax.swing.JButton btnProizvodi;
+    private javax.swing.JButton btnRacuni;
     private javax.swing.JToolBar jToolBar1;
+    private javax.swing.JLabel lblNaslov;
     private javax.swing.JLabel lblOperater;
     // End of variables declaration//GEN-END:variables
 }
